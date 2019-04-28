0c3cb3fcef7ee60b0062cadff23e541a17a56011
Bella Sherring <isabella.sherring@uon.edu.au>
2019-04-28 00:27:10 +0000

    practicing rotaitonal decryption of the unsen

--- practice.c
+++ practice.c
@@ -2,18 +2,25 @@
 #include <stdlib.h>
 #include <string.h>
 
+char rotdecrypt(char *phrase, float rotationKey);
+
 int main()
 {
     //initialising variables
-    char phrase[300];
-    int i=0;
+    FILE *input; //declares file which will be referred to as 'input'
+    input = fopen("practicecode.txt", "r");
+    
+    char phrase[30000];
+    char i=0;
+    char letter = 0;
     char x=0;
     int countA=0, countB=0, countC=0, countD=0, countE=0, countF=0, countG=0, countH=0, countI=0, countJ=0, countK=0, countL=0, countM=0, countN=0, countO=0, countP=0, countQ=0, countR=0, countS=0, countT=0, countU=0, countV=0, countW=0, countX=0, countY=0, countZ=0;
     int data[26];
+    float rotationKey;
     
     //gets the phrase from the user
-    printf("Phrase: \n");
-    scanf("%[^\n]s", phrase);
+    //printf("Phrase: \n");
+    fscanf(input, "%[^\n]s", phrase);
     
     //counts the amount of each letter in the phrase
     while(phrase[i] != '\0')
@@ -115,19 +122,49 @@ int main()
     
     int maximum=-1e30;
     int maxindex=0;
-    i=0;
+    letter=0;
     
-    while(i<26)  //while the elements don't equal 0, a new line
+    while(letter<26)  //while the elements don't equal 0, a new line
     {
-        if(data[i]>maximum)
+        if(data[letter]>maximum)
         {
-            maximum=data[i];    //the new maximum is assigned to the element number i
-            maxindex=i;     //max index is set as i
+            maximum=data[letter];    //the new maximum is assigned to the element number i
+            maxindex=letter;     //max index is set as i
         }
-        i++;    //add another
+        letter++;    //add another
     }
     
-    printf("%d at index %d\n", maximum, maxindex); //testing that the maximum finds the right number
+    //printf("%d at index %d\n", maximum, maxindex); //testing that the maximum finds the right number
+    printf("If T is the most common letter in the decryption, the decryption will be: \n");
+    rotationKey = (maxindex+65) - 'T' + 26;
+    rotdecrypt(phrase, rotationKey);
+    printf("\nIf A is the most common letter in the decryption, the decryption will be: \n");
+    rotationKey= (maxindex+65) - 'A' + 26;
+    rotdecrypt(phrase, rotationKey);
     
     return 0;
+}
+
+char rotdecrypt(char *phrase, float rotationKey)
+{
+    char i=0; //i is the variable that will count through each individual character of the phrase string. It is initialised as 0 to start at the beginning of the string.
+    char x;
+    while(phrase[i] != 0) //the content of this while loop will continue to execute until the computer finds that the character 'phrase[i]' is equal to 0, because then it no longer fits the condition of != 0 (not equal to 0). It will occur until a new line is reached.
+    {
+        x=phrase[i];
+        if(x<=64 || x>=91) //if the value of the phrase[i] character in the original plain English text message is something other than a capital letter, this block of code will execute
+        {
+            x = x; //phrase[i] remains unchanged
+        }  
+        else if((x-rotationKey)>=65 && (x-rotationKey)<=90) //if the new decrypted ASCII falls within the capital letter range of 65 and 90 (i.e. it remains a capital letter not a different ASCII character that isn't English) this block of code will execute
+        {
+            x = x - rotationKey; //the ASCII value of phrase[i] is shifted back by the rotationKey in order to decrypt it into the plain English ASCII character
+        }
+        else if((x-rotationKey)<65) //if the new decrypted ASCII value falls below the capital letter range
+        {
+            x = x + 26 - rotationKey; //phrase[i] is shifted back by the rotationKey, but 26 is added (the number of letters in the alphabet) so that it stays within the capital letter range
+        }
+        printf("%c", x); //prints the decrypted letter to the screen (stdout)
+        i++;
+    }
 }
